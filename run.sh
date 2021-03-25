082398a9c5af7837945902c085c3d37f4eb06270
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2021-03-26 00:42:10 +0900

    comment sshd

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 test -z $(which ansible-playbook) && sudo dnf install -y epel-release && sudo dnf install -y ansible
 sudo dnf update -y ansible
 ansible-playbook main.yml --connection=local
-ansible-playbook 00-sshd.yml --connection=local
+#ansible-playbook 00-sshd.yml --connection=local
 ansible-playbook 01-apache.yml --connection=local
 ansible-playbook 04-mecab.yml --connection=local
 ansible-playbook 05-mariadb.yml --connection=local
